8fad19d6afb1076f614e7faf750c1d1f6f259a9b
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-02-23 01:47:36 +0100

    Student model firstname custom validator

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -13,6 +13,13 @@ const userNameSchema = new Schema<UserName>({
     required: [true, 'First Name is required'],
     trim: true,
     maxlength: [20, 'First Name can not be more than 20 characters'],
+    validate: {
+      validator: function (value: string) {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+        return firstNameStr === value;
+      },
+      message: '{VALUE} is not in capitalize format',
+    },
   },
   middleName: {
     type: String,
