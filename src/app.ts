e76d584b85575fb063b3db7921d29f056886aa49
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-02-28 01:37:08 +0100

    Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app: Application = express();
 
 // Parsers
@@ -15,5 +16,7 @@ app.use('/api/v1/users', UserRoutes);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use(globalErrorHandler);
 
 export default app;
