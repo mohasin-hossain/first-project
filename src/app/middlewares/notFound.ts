0d03bb02bf4bdedcddc92a6ea5d9281318c6e8fa
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-01 12:44:35 +0100

    Send Response handler

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
-import htttpStatus from 'http-status';
+import httpStatus from 'http-status';
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(htttpStatus.NOT_FOUND).json({
+  return res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'API Not Found !!',
     error: '',
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './user.service';
+import sendResponse from '../../utils/sendResponses';
+import httpStatus from 'http-status';
 
 const createStudent = async (
   req: Request,
@@ -18,8 +20,8 @@ const createStudent = async (
       studentsData,
     );
 
-    // Send response
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'Student is created successfully',
       data: result,
