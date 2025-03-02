97545510437db1f106907012adddfd82e7e4732d
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-02 09:59:24 +0100

    REFACTOR: creating academicSemester.constant.ts for reusable models

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,23 +1,7 @@
 import { Model, Schema, model } from 'mongoose';
 import { TAcademicSemester, TAcademicSemesterCode, TAcademicSemesterName, TMonths } from './academicSemester.interface';
+import { AcademicSemesterCode, AcademicSemesterName, Months } from './academicSemester.constant';
 
-const Months: TMonths[] = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-const AcademicSemesterName: TAcademicSemesterName[] = ['Autumn', 'Summer', 'Fall'];
-const AcademicSemesterCode: TAcademicSemesterCode[] = ['01', '02', '03'];
 
 const academicSemesterSchema = new Schema<TAcademicSemester>(
   {
