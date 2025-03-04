552eadf6e317270b71c77e5a3de25deccdcdb2c5
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2025-03-05 00:35:03 +0100

    [Bug Fix]: Starting ID count from 0000 for new semester and new year

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,16 +14,29 @@ const findLastStudnetId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // Year semesterCode 4 digit number
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // First time 0000
-  const currentId =
-    (await findLastStudnetId()) || (0).toString().padStart(4, '0');
+  let currentId = (0).toString(); // 0000 by default
 
-  // Then increment
+  const lastStudentId = await findLastStudnetId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
